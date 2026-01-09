cfbb0d6b1657ff5f2332cd39d28ab8c1ea213da3
Kathryn AV Dambeck <audiovisual@dambeck.org>
2026-01-09 13:04:16 -0500

    removed decimals, takes user input

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-orm = 100
+orm = int(input("What is your ORM?\n"))
 
 # The outer array is the full schedule. The nessted array is each week. Each value is a set.
 template = [
@@ -29,9 +29,9 @@ Week 4\t{schedule[3][1]}\t{schedule[3][1]} \t{schedule[3][2]}
 
 output_vert = f"""
 \tWeek 1\t Week 2\tWeek 3\tWeek 4
-Set 1\t{schedule[0][0]}x5\t{schedule[1][0]}x3\t{schedule[2][0]}x5\t{schedule[3][0]}x5
-Set 2\t{schedule[0][1]}x5\t{schedule[1][1]}x3\t{schedule[2][1]}x3\t{schedule[3][1]}x5
-Set 3\t{schedule[0][2]}x5\t{schedule[1][2]}x3\t{schedule[2][2]}x1\t{schedule[3][2]}x5
+Set 1\t{int(schedule[0][0])}x5\t{int(schedule[1][0])}x3\t{int(schedule[2][0])}x5\t{int(schedule[3][0])}x5
+Set 2\t{int(schedule[0][1])}x5\t{int(schedule[1][1])}x3\t{int(schedule[2][1])}x3\t{int(schedule[3][1])}x5
+Set 3\t{int(schedule[0][2])}x5\t{int(schedule[1][2])}x3\t{int(schedule[2][2])}x1\t{int(schedule[3][2])}x5
 """
 
 print(output_vert)
