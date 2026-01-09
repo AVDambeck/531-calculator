5570b54ae2909246b5efe1d1c19fecb0d2b0cd0f
Kathryn AV Dambeck <audiovisual@dambeck.org>
2026-01-09 12:58:32 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 531-calculator
 simple 5/3/1 calculator
+
+Jim Wendler's 5/3/1 program is pretty popular. This is just a python script that will take you ORM and then give you the lifts for four weeks.
---
+++ calc.py
@@ -0,0 +1,40 @@
+orm = 100
+
+# The outer array is the full schedule. The nessted array is each week. Each value is a set.
+template = [
+        [0.65, 0.75, 0.85],
+        [0.7, 0.8, 0.9],
+        [0.75, 0.85, 0.95],
+        [0.4, 0.5, 0.6]
+    ]
+
+schedule = []
+
+for week in template:
+    ls = []
+    for lift in week:
+        weight = lift * orm
+        ls.append(weight)
+    schedule.append(ls)
+
+# Output
+
+output_hort = f"""
+\tSet 1\tSet 2\tSet3
+Week 1\t{schedule[0][0]}\t{schedule[0][1]} \t{schedule[0][2]} 
+Week 2\t{schedule[1][1]}\t{schedule[1][1]} \t{schedule[1][2]} 
+Week 3\t{schedule[2][1]}\t{schedule[2][1]} \t{schedule[2][2]} 
+Week 4\t{schedule[3][1]}\t{schedule[3][1]} \t{schedule[3][2]} 
+"""
+
+output_vert = f"""
+\tWeek 1\t Week 2\tWeek 3\tWeek 4
+Set 1\t{schedule[0][0]}x5\t{schedule[1][0]}x3\t{schedule[2][0]}x5\t{schedule[3][0]}x5
+Set 2\t{schedule[0][1]}x5\t{schedule[1][1]}x3\t{schedule[2][1]}x3\t{schedule[3][1]}x5
+Set 3\t{schedule[0][2]}x5\t{schedule[1][2]}x3\t{schedule[2][2]}x1\t{schedule[3][2]}x5
+"""
+
+print(output_vert)
+
+
+
